39487b516065c3f9497d0d3e09003674c91e8b6c
Evgeniy K <elv1s42k@gmail.com>
2023-10-19 19:28:16 +0300

    chip

--- components/pages/ChipPage.tsx
+++ components/pages/ChipPage.tsx
@@ -13,10 +13,18 @@ export const ChipPage = (props) => {
       <Text xl>Chip sizes:</Text>
       <Chip xs>Extra small</Chip> <Chip sm>Small size</Chip> <Chip md>Medium size</Chip> <Chip lg>Large size</Chip> <Chip xl>Extra large</Chip>
       <Divider lg />
-      <Text xl>Chip colors:</Text>
+      <Text xl>Default chips:</Text>
       <Chip primary>Primary chip</Chip> <Chip secondary>Secondary chip</Chip>
       <Divider sm noborder />
       <Chip success>Success chip</Chip> <Chip info>Info chip</Chip> <Chip warning>Warning chip</Chip> <Chip error>Error chip</Chip>
+      <Text xl>Filled chips:</Text>
+      <Chip primary filled>Primary chip</Chip> <Chip secondary filled>Secondary chip</Chip>
+      <Divider sm noborder />
+      <Chip success filled>Success chip</Chip> <Chip info filled>Info chip</Chip> <Chip warning filled>Warning chip</Chip> <Chip error filled>Error chip</Chip>
+      <Text xl>Outline chips:</Text>
+      <Chip primary outline>Primary chip</Chip> <Chip secondary outline>Secondary chip</Chip>
+      <Divider sm noborder />
+      <Chip success outline>Success chip</Chip> <Chip info outline>Info chip</Chip> <Chip warning outline>Warning chip</Chip> <Chip error outline>Error chip</Chip>
     </CommonPage>
   );
 };
--- components/ui/simple/Chip.tsx
+++ components/ui/simple/Chip.tsx
@@ -2,23 +2,70 @@ import React from 'react';
 import { ChipProps } from '../props/Props';
 import { buildSimpleComponent } from '../utils';
 
-export const Chip: React.FC<ChipProps> = (props) => buildSimpleComponent(props, "span", [], [
-  "font-family-code text-decoration-none",
-  ['fs-2xs', 'fs-xs', 'fs-sm', 'fs-md', 'fs-lg'],
-  ['br-1', 'br-2', 'br-3', 'br-3', 'br-4'],
-  ['py-0', 'py-1', 'py-1', 'py-2', 'py-3'],
-  ['px-2', 'px-2', 'px-3', 'px-3', 'px-4'],
-  props.bold ? 'fw-bold'
-    : props.normal ? 'fw-normal'
-      : 'fw-bold',
-  props.filled ? 'border-none'
-    : props.outline ? 'border-1'
-      : 'border-1',
-  props.primary ? 'bg-color-primary'
-    : props.secondary ? 'bg-color-secondary'
-      : props.success ? 'bg-color-success'
-        : props.info ? 'bg-color-info'
-          : props.warning ? 'bg-color-warning'
-            : props.error ? 'bg-color-error'
-              : 'bg-color-grey-50'
-]);
+export const Chip: React.FC<ChipProps> = (props) => {
+  const outlineChipClasses = [
+    props.primary ? 'color-primary'
+      : props.secondary ? 'color-secondary'
+        : props.success ? 'color-success'
+          : props.info ? 'color-info'
+            : props.warning ? 'color-warning'
+              : props.error ? 'color-error'
+                : 'color-text',
+    props.primary ? 'bg-color-bg-primary'
+      : props.secondary ? 'bg-color-bg-secondary'
+        : props.success ? 'bg-color-bg-success'
+          : props.info ? 'bg-color-bg-info'
+            : props.warning ? 'bg-color-bg-warning'
+              : props.error ? 'bg-color-bg-error'
+                : 'bg-color-grey-50',
+    props.primary ? 'border-color-border-primary'
+      : props.secondary ? 'border-color-border-secondary'
+        : props.success ? 'border-color-border-success'
+          : props.info ? 'border-color-border-info'
+            : props.warning ? 'border-color-border-warning'
+              : props.error ? 'border-color-border-error'
+                : 'border-color-border'
+  ];
+  const filledChipClasses = [
+    'color-bg',    
+    //props.primary ? 'color-bg-primary'
+    //  : props.secondary ? 'color-bg-secondary'
+    //    : props.success ? 'color-bg-success'
+    //      : props.info ? 'color-bg-info'
+    //        : props.warning ? 'color-bg-warning'
+    //          : props.error ? 'color-bg-error'
+    //            : 'color-bg-secondary',
+    props.primary ? 'bg-color-primary'
+      : props.secondary ? 'bg-color-secondary'
+        : props.success ? 'bg-color-success'
+          : props.info ? 'bg-color-info'
+            : props.warning ? 'bg-color-warning'
+              : props.error ? 'bg-color-error'
+                : 'bg-color-text',
+    props.primary ? 'border-color-primary'
+      : props.secondary ? 'border-color-secondary'
+        : props.success ? 'border-color-success'
+          : props.info ? 'border-color-info'
+            : props.warning ? 'border-color-warning'
+              : props.error ? 'border-color-error'
+                : 'border-color-bg'
+  ];
+  const other = props.filled ? filledChipClasses
+    : props.outline ? outlineChipClasses
+      : outlineChipClasses;
+
+  return buildSimpleComponent(props, "span", [], [
+    "font-family-code text-decoration-none border-style-solid",
+    ['fs-2xs', 'fs-xs', 'fs-sm', 'fs-md', 'fs-lg'],
+    ['br-1', 'br-2', 'br-3', 'br-3', 'br-4'],
+    ['py-0', 'py-1', 'py-1', 'py-2', 'py-3'],
+    ['px-2', 'px-2', 'px-3', 'px-3', 'px-4'],
+    props.bold ? 'fw-bold'
+      : props.normal ? 'fw-normal'
+        : 'fw-500',
+    props.filled ? 'border-none'
+      : props.outline ? 'border-width-1px'
+        : 'border-width-1px',
+    ...other
+  ])
+};
