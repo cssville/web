be59ee1c7211406a2a32c643d6526ad18b42f7fa
Evgeniy Kosjakov <elv1s42k@gmail.com>
2024-08-29 18:17:43 +0300

    small changes

--- components/CssClassesSection.tsx
+++ components/CssClassesSection.tsx
@@ -1,12 +1,12 @@
-import React, { useMemo } from 'react';
-import { ReactNode } from "react";
-import { Cssville } from 'cssville-generators/build/cssville';
-import { Display, Text, Title } from 'cssville-ui/build/components/ui/simple/Typography';
-import { Stack } from 'cssville-ui/build/components/ui/simple/Stack';
-import { Section } from "cssville-ui/build/components/ui/simple/Section";
+import React from 'react';
+import {ReactNode} from "react";
+import {Cssville} from 'cssville-generators/build/cssville';
+import {Display, Text, Title} from 'cssville-ui/build/components/ui/simple/Typography';
+import {Stack} from 'cssville-ui/build/components/ui/simple/Stack';
+import {Section} from "cssville-ui/build/components/ui/simple/Section";
 
 
-export const CssClassesSection = (props: any) => {
+export const CssClassesSection = () => {
   const generatorNodes: ReactNode[] = Cssville.generators.map((g, i) => {
     return (
       <Stack column className="wid-full" key={`node-${g.name}-${i}`}>
@@ -15,12 +15,12 @@ export const CssClassesSection = (props: any) => {
           g.cssData.map(d =>
             <div key={`node-${g.name}-${i}-${d.className}`}>
               {d.className}: {d.cssProperties.map(
-                (p, j) =>
-                  <React.Fragment key={`node-${g.name}-${j}-${p}`}>
-                    <span>{p}</span>
-                    {j < d.cssProperties.length - 1 && <span>, </span>}
-                  </React.Fragment>
-              )}
+              (p, j) =>
+                <React.Fragment key={`node-${g.name}-${j}-${p}`}>
+                  <span>{p}</span>
+                  {j < d.cssProperties.length - 1 && <span>, </span>}
+                </React.Fragment>
+            )}
             </div>
           )
         }
@@ -37,7 +37,8 @@ export const CssClassesSection = (props: any) => {
         <Text lg className="max-wid-2/3 md-max-wid-full">
           Utility classes are a specific type of class that is designed to provide a single,
           focused styling property, such as padding, margin, or font size. Unlike traditional classes,
-          which might be used to define more complex styles, utility classes are very simple and focused on a single aspect of the style.
+          which might be used to define more complex styles, utility classes are very simple and focused on a
+          single aspect of the style.
         </Text>
         {generatorNodes}
       </Stack>
